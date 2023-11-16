06036b24bb8bac8e4c484f601fc0776fb9af7081
Joao Sagorro <joao.sagorro@gmail.com>
2023-11-16 08:57:08 +0000

    not finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts 'Cleaning your database'
+Flat.destroy_all
+
+puts '...'
+
+puts 'Planting seeds'
+
+30.times do
+  new_flats = Flat.new({
+    name: Faker::JapaneseMedia::DragonBall.character,
+    address: Faker::Address.full_address,
+    description: Faker::ChuckNorris.fact,
+    price_per_night: "#{Faker::Number.decimal(l_digits: 3, r_digits: 2)} $$",
+    number_of_guests: Faker::Number.number(digits: 2)
+  })
+
+  new_flats.save
+  puts 'One flat created'
+end
+
+puts 'All done, congrats'
